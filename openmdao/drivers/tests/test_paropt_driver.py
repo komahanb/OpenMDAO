7462022a01132cf7816ea3dbbbbfc886ce0ba9f8
komahan <komahan.cool@gmail.com>
2018-02-20 14:40:23 -0500

    worked on constructor for Driver; added test program file

---
+++ openmdao/drivers/tests/test_paropt_driver.py
@@ -0,0 +1,59 @@
+""" Unit tests for the Paropt Driver."""
+
+import sys
+import unittest
+import numpy as np
+
+from openmdao.api import Problem, Group, IndepVarComp, ExecComp, AnalysisError, ExplicitComponent, \
+    ScipyKrylov, NonlinearBlockGS, LinearBlockGS
+from openmdao.utils.assert_utils import assert_rel_error
+from openmdao.test_suite.components.paraboloid import Paraboloid
+from openmdao.test_suite.components.expl_comp_array import TestExplCompArrayDense
+from openmdao.test_suite.components.sellar import SellarDerivativesGrouped
+from openmdao.utils.general_utils import set_pyoptsparse_opt
+
+class TestPyoptSparse(unittest.TestCase):
+
+    def setUp(self):
+        if OPT is None:
+            raise unittest.SkipTest("pyoptsparse is not installed")
+
+        if OPTIMIZER is None:
+            raise unittest.SkipTest("pyoptsparse is not providing SNOPT or SLSQP")
+
+    def test_simple_paraboloid_upper(self):
+
+        prob = Problem()
+        model = prob.model = Group()
+
+        model.add_subsystem('p1', IndepVarComp('x', 50.0), promotes=['*'])
+        model.add_subsystem('p2', IndepVarComp('y', 50.0), promotes=['*'])
+        model.add_subsystem('comp', Paraboloid(), promotes=['*'])
+        model.add_subsystem('con', ExecComp('c = - x + y'), promotes=['*'])
+
+        prob.set_solver_print(level=0)
+
+        prob.driver = pyOptSparseDriver()
+        prob.driver.options['optimizer'] = OPTIMIZER
+        if OPTIMIZER == 'SLSQP':
+            prob.driver.opt_settings['ACC'] = 1e-9
+        prob.driver.options['print_results'] = False
+
+        model.add_design_var('x', lower=-50.0, upper=50.0)
+        model.add_design_var('y', lower=-50.0, upper=50.0)
+        model.add_objective('f_xy')
+        model.add_constraint('c', upper=-15.0)
+
+        prob.setup(check=False)
+
+        failed = prob.run_driver()
+
+        self.assertFalse(failed, "Optimization failed, info = " +
+                                 str(prob.driver.pyopt_solution.optInform))
+
+        # Minimum should be at (7.166667, -7.833334)
+        assert_rel_error(self, prob['x'], 7.16667, 1e-6)
+        assert_rel_error(self, prob['y'], -7.833334, 1e-6)
+
+if __name__ == "__main__":
+    unittest.main()
